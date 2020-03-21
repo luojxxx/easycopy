8d66d3c463f565bd0f831b57869e2f0f6878ee12
luojxxx <luojxxx@users.noreply.github.com>
2020-03-21 15:58:43 -0700

    Add basic contact page copy

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -4,11 +4,13 @@ import { Text } from "rebass";
 import Template from "./components/Template";
 import Textarea from "./components/Textarea";
 
+const text = "email: contact@easycopy.com"
+
 const Contact = () => (
   <Template subheading="Contact">
     <Textarea
       type="text"
-      value={"Placeholder text"}
+      value={text}
       readOnly
       style={{
         minHeight: "25vh"
