f1548ea9576a6640df3c72e05f292cbce1ad9cc4
luojxxx <luojxxx@users.noreply.github.com>
2020-03-21 16:30:36 -0700

    Style textarea scrollbars

--- frontend/src/components/Textarea.js
+++ frontend/src/components/Textarea.js
@@ -16,6 +16,30 @@ const TextareaComponent = styled(Textarea)`
     -webkit-transition-delay: 9999s;
     -webkit-transition: color 9999s ease-out, background-color 9999s ease-out;
   }
+
+  ::-webkit-scrollbar {
+    width: 9px;
+  }
+
+  ::-webkit-scrollbar-track {
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    background: rgba(0, 0, 0, 0.0);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: rgba(255, 255, 255, 0.6);
+  }
+
+  ::-webkit-scrollbar-thumb:window-inactive {
+    background: rgba(255, 255, 255, 0.2);
+  }
 `;
 
 export default TextareaComponent;
