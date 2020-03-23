bfdb4ab93a18f53139dbb7f191685d1636004a95
luojxxx <luojxxx@users.noreply.github.com>
2020-03-23 14:55:05 -0700

    Modify frontend env variable to use REACT_APP_ prefix

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-const api = process.env.DEBUG === 'true' ? "http://localhost:3000" : "https://easycopy.herokuapp.com"
+const api = process.env.REACT_APP_DEBUG === 'true' ? "http://localhost:3000" : "https://easycopy.herokuapp.com"
 const contentLimit = 10000;
 
 export default {
