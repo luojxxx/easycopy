ca999f869f59276aa64f2e670c4025fee88879ad
luojxxx <luojxxx@users.noreply.github.com>
2020-03-25 16:23:36 -0700

    Update contact email

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 import Template from "./components/Template";
 import Textarea from "./components/Textarea";
 
-const text = "email: contact@easycopy.com"
+const text = "email: contact@quickshift.io"
 
 const Contact = () => (
   <Template subheading="Contact">
