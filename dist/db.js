fb745d3ac4a9dd180200e13b6871832125efdc23
luojxxx <luojxxx@users.noreply.github.com>
2020-07-05 17:02:48 -0700

    Comment out DB sync

--- dist/db.js
+++ dist/db.js
@@ -37,28 +37,23 @@ var dbCheck = /*#__PURE__*/function () {
             return db.authenticate();
 
           case 3:
-            console.log("Connection has been established successfully.");
-            _context.next = 6;
-            return db.sync({
-              force: process.env.DB_SYNC
-            });
+            console.log("Connection has been established successfully."); // await db.sync({ force: true });
+            // console.log("Models are synchronized");
 
-          case 6:
-            console.log("Models are synchronized");
-            _context.next = 12;
+            _context.next = 9;
             break;
 
-          case 9:
-            _context.prev = 9;
+          case 6:
+            _context.prev = 6;
             _context.t0 = _context["catch"](0);
             console.error("Unable to connect to the database:", _context.t0);
 
-          case 12:
+          case 9:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[0, 9]]);
+    }, _callee, null, [[0, 6]]);
   }));
 
   return function dbCheck() {
