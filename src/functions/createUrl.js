44570759bedc1f9f61cc2277c6edefdd6d6a82d7
luojxxx <luojxxx@users.noreply.github.com>
2020-07-15 16:53:07 -0700

    Separate generateWordArray function

--- src/functions/createUrl.js
+++ src/functions/createUrl.js
@@ -10,20 +10,20 @@ const text = fs.readFileSync(
 );
 const wordBank = text.trim("\n").split(",");
 
+const generateWordArray = () => {
+  return [0, 0, 0, 0].map((_) => {
+    const idx = Math.floor(Math.random() * wordBank.length);
+    const word = wordBank[idx];
+    return word[0].toUpperCase() + word.substring(1);
+  });
+};
+
 export const createUrl = async (content, userName, type) => {
   // make sure url doesn't already exist
-  let wordArray
   let url;
   let results = true;
   while (results) {
-    wordArray = [0, 0, 0, 0].map(
-      _ => {
-        const idx = Math.floor(Math.random() * wordBank.length)
-        const word = wordBank[idx]
-        return word[0].toUpperCase() + word.substring(1,)
-    }
-    );
-    url = wordArray.join("");
+    url = generateWordArray().join("");
     results = await Url.findOne({ where: { urlRaw : url.toLowerCase() } });
   }
 
