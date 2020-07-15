d0c01f7e3f680dcbb76ac64e10ec651a7de07df4
luojxxx <luojxxx@users.noreply.github.com>
2020-07-15 11:55:35 -0700

    Create test skeleton

--- test/test.js
+++ test/test.js
@@ -6,3 +6,83 @@ describe('Array', function () {
     });
   });
 });
+
+describe("Account Management", function () {
+  describe("signup", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("login", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("verifyEmail", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("resendVerifyEmail", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("changeEmail", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("changePassword", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("deleteAccount", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("Account Features", function () {
+  describe("getUserUrls", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("deleteUserUrl", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("Url creation/retrieval", function () {
+  describe("create", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe("*", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("Recaptcha", function () {
+  describe("verifyRecaptcha", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("Payment features", function () {
+  describe("donate", function () {
+    it("should return", function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
