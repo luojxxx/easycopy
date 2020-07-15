75840a2ede6a0de8819f4b798a410425dab83d30
luojxxx <luojxxx@users.noreply.github.com>
2020-07-15 16:52:35 -0700

    Setup MockDB with local postgres DB

--- test/MockDB.js
+++ test/MockDB.js
@@ -1,12 +1,44 @@
+require("dotenv").config();
+import CryptoJS from "crypto-js";
+
+import db from './../src/db'
 import User from '../src/model/User'
 import UserToken from '../src/model/UserToken'
 import EmailVerificationToken from '../src/model/EmailVerificationToken'
 import Url from '../src/model/Url'
 
-export const DBSetup = () => {
-  //
+export const DBSetup = async () => {
+  await Url.create({
+    url: 'HappenRockEffortTrain',
+    urlRaw: 'HappenRockEffortTrain'.toLowerCase(),
+    content: CryptoJS.AES.encrypt(
+      'Test Content',
+      process.env.ENCRYPTION_KEY
+    ).toString(),
+    userName: 'TestUser',
+    type: 'text',
+  });
+  await User.create({
+    userName: "cloudlife",
+    email: "luojx2010@gmail.com",
+    password: "saltedpassword",
+    emailVerifying: null,
+    emailVerified: true,
+    createdAt: Date.now(),
+  });
 }
 
-export const DBClear = () => {
-  //
+export const DBClear = async () => {
+  await Url.destroy({
+    where: {},
+  });
+  await EmailVerificationToken.destroy({
+    where: {},
+  });
+  await UserToken.destroy({
+    where: {}
+  });
+  await User.destroy({
+    where: {},
+  });
 }
