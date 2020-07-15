37707cd7e27548c5fc19a37f1fc3add6784818aa
luojxxx <luojxxx@users.noreply.github.com>
2020-07-15 07:37:31 -0700

    Disable stripe temporarily

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as Sentry from "@sentry/browser";
 import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
 import App from "./views/App";
 import Contact from "./views/Contact";
@@ -19,22 +19,22 @@ import * as serviceWorker from "./serviceWorker";
 Sentry.init({
   dsn: "https://83b0d8e1cf934f2aa72aa89f194a5732@sentry.io/5169583"
 });
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const Index = (
-  <Elements stripe={stripePromise}>
+  // <Elements stripe={stripePromise}>
     <Router>
       <ThemeProvider theme={theme}>
         <Switch>
           <Route path="/contact" component={Contact} />
           <Route path="/privacy" component={Privacy} />
           <Route path="/termsofservice" component={TermsOfService} />
-          <Route path="/donate" component={Donate} />
+          {/* <Route path="/donate" component={Donate} /> */}
           <Route path="/" component={App} />
         </Switch>
       </ThemeProvider>
     </Router>
-  </Elements>
+  // </Elements>
 );
 
 ReactDOM.render(Index, document.getElementById("root"));
