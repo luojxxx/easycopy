2a454f70da54cd17eb97fe5b453b90e13f6f683c
luojxxx <luojxxx@users.noreply.github.com>
2020-07-16 07:37:05 -0700

    Write initial UrlTestSuite

--- test/testSuites/Urls/index.js
+++ test/testSuites/Urls/index.js
@@ -1,6 +1,9 @@
-const assert = require("assert");
+const expect = require("chai").expect;
 
 import { DBSetup, DBClear } from "../../MockDB";
+
+import { getUrl } from '../../../src/functions/getUrl'
+import { createUrl } from "../../../src/functions/createUrl";
 
 const UrlTestSuite = () => {
   describe("Url creation/retrieval", function () {
@@ -8,15 +11,35 @@ const UrlTestSuite = () => {
       await DBSetup();
     });
 
-    describe("create", function () {
-      it("should return", function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
+    describe("retrieve url", function () {
+      it("should be able to retrieve the url in the mock DB (case insensitive)", async function () {
+        const url = "HappenRockEffortTRAIN";
+        const { body } = await getUrl(url)
+        expect(body).to.be.a("object");
+        expect(body).to.have.property("url").equal("HappenRockEffortTrain");
+        expect(body).to.have.property("userName").equal('TestUser');
+        expect(body).to.have.property("content").equal('Test Content');
+        expect(body).to.have.property("type").equal('text');
       });
     });
-    describe("*", function () {
-      it("should return", function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
+    describe("create text url and retrieve it", function () {
+      const userName = 'BobTestDummy'
+      const content = 'I hope todays test goes well'
+      const type = 'text'
+      let url
+      it("should create a url", async function () {
+        const { body } = await createUrl(content, userName, type);
+        url = body.url
+        expect(url).to.be.a('string')
       });
+      it("should retrieve the created url", async function () {
+        const { body } = await getUrl(url)
+        expect(body).to.be.a("object");
+        expect(body).to.have.property("url").equal(url);
+        expect(body).to.have.property("userName").equal(userName);
+        expect(body).to.have.property("content").equal(content);
+        expect(body).to.have.property("type").equal(type);
+      })
     });
 
     after(async function () {
