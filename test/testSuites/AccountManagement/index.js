709db263a527eff2aa1ee68fe8a4f54abba8e97b
luojxxx <luojxxx@users.noreply.github.com>
2020-07-16 16:42:31 -0700

    Add changePassword function and tests

--- test/testSuites/AccountManagement/index.js
+++ test/testSuites/AccountManagement/index.js
@@ -5,6 +5,7 @@ import signup from './signup'
 import login from './login'
 import sendVerifyEmail from './sendVerifyEmail'
 import changeEmail from './changeEmail'
+import changePassword from './changePassword'
 
 const AccountManagementTests = () => {
   describe("Account Management", function () {
@@ -16,12 +17,8 @@ const AccountManagementTests = () => {
     login();
     sendVerifyEmail();
     changeEmail()
+    changePassword()
     describe("verifyEmail", function () {
-      it("should return", function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-      });
-    });
-    describe("changePassword", function () {
       it("should return", function () {
         assert.equal([1, 2, 3].indexOf(4), -1);
       });
