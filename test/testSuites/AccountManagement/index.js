a841c90e16b9b0a84dc840a0e59b71790a75c84a
luojxxx <luojxxx@users.noreply.github.com>
2020-07-16 12:08:06 -0700

    Add tests for sendVerificationEmail

--- test/testSuites/AccountManagement/index.js
+++ test/testSuites/AccountManagement/index.js
@@ -3,21 +3,18 @@ const assert = require("assert");
 import { DBSetup, DBClear } from "../../MockDB";
 import signup from './signup'
 import login from './login'
+import sendVerifyEmail from './sendVerifyEmail'
 
 const AccountManagementTests = () => {
-  describe("Url creation/retrieval", function () {
+  describe("Account Management", function () {
     before(async function () {
       await DBSetup();
     });
 
     signup();
     login();
+    sendVerifyEmail();
     describe("verifyEmail", function () {
-      it("should return", function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-      });
-    });
-    describe("resendVerifyEmail", function () {
       it("should return", function () {
         assert.equal([1, 2, 3].indexOf(4), -1);
       });
---
+++ test/testSuites/AccountManagement/sendVerifyEmail.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+
+import { sendVerificationEmail } from "../../../src/functions/sendVerificationEmail";
+import { UserVerified, UserUnverified } from "../../MockDB";
+import EmailVerificationToken from "../../../src/model/EmailVerificationToken";
+
+const SendVerificationEmailTests = () => {
+  describe("sendVerificationEmail", async function () {
+    const userId = UserUnverified.userId;
+    const email = UserUnverified.emailVerifying;
+
+    let emailToken1;
+    it("sends a verification email", async function () {
+      const result = await sendVerificationEmail(userId, email);
+      const newEmailVerificationToken = await EmailVerificationToken.findOne({
+        where: { userId: userId },
+      });
+      emailToken1 = newEmailVerificationToken.verificationToken;
+    });
+    it("resends a verification email and updates email token", async function () {
+      const result = await sendVerificationEmail(userId, email);
+      const newEmailVerificationToken = await EmailVerificationToken.findOne({
+        where: { userId: userId },
+      });
+      expect(emailToken1).to.not.be.equal(
+        newEmailVerificationToken.verificationToken
+      );
+    });
+  });
+};
+
+export default SendVerificationEmailTests;
