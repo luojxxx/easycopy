5ae9dd7bc95d28ec2111ab6e518f7aae0b460d49
luojxxx <luojxxx@users.noreply.github.com>
2020-07-16 17:35:28 -0700

    Format code

--- test/testSuites/AccountManagement/deleteAccount.js
+++ test/testSuites/AccountManagement/deleteAccount.js
@@ -2,8 +2,8 @@ import { expect } from "chai";
 
 import { deleteAccount } from "../../../src/functions/deleteAccount";
 import User from "../../../src/model/User";
-import UserToken from '../../../src/model/UserToken'
-import EmailVerificationToken from '../../../src/model/EmailVerificationToken'
+import UserToken from "../../../src/model/UserToken";
+import EmailVerificationToken from "../../../src/model/EmailVerificationToken";
 import { UserVerified, UserUnverified } from "../../MockDB";
 
 import { hashString } from "../../../src/lib";
@@ -13,24 +13,26 @@ const DeleteAccountTests = () => {
     it("should error when given wrong old password", async function () {
       const { status } = await deleteAccount(
         UserVerified.userId,
-        "wrongPassword",
+        "wrongPassword"
       );
       expect(status).to.be.equal(401);
     });
     it("should successfully scrub account from DB", async function () {
       const { status } = await deleteAccount(
         UserVerified.userId,
-        UserVerified.password,
+        UserVerified.password
       );
       const userResult = await User.findOne({
-        where: { userId: UserVerified.userId }
-      })
+        where: { userId: UserVerified.userId },
+      });
       const userTokenResult = await UserToken.findOne({
         where: { userId: UserVerified.userId },
       });
-      const emailVerificationTokenResult = await EmailVerificationToken.findOne({
-        where: { userId: UserVerified.userId },
-      });
+      const emailVerificationTokenResult = await EmailVerificationToken.findOne(
+        {
+          where: { userId: UserVerified.userId },
+        }
+      );
       expect(status).to.be.equal(200);
       expect(userResult).to.be.equal(null);
       expect(userTokenResult).to.be.equal(null);
