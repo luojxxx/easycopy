2e51ed4715657fb6adcce79ba1d1e75b8ef888f3
luojxxx <luojxxx@users.noreply.github.com>
2020-08-05 13:55:25 -0700

    Format db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 import { Sequelize } from "sequelize";
+import { urlencoded } from "express";
 
-const SSL = process.env.DB_SSL_OFF ? {} : {
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-}
+const SSL = process.env.DB_SSL_OFF
+  ? {}
+  : {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    };
 
 const db = new Sequelize(process.env.DB_STRING, {
   dialect: "postgres",
@@ -19,20 +22,20 @@ const dbCheck = async () => {
   try {
     await db.authenticate();
     console.log("Connection has been established successfully.");
-    if (process.env.DB_SYNC === 'true') {
+    if (process.env.DB_SYNC === "true") {
       try {
         await db.sync({ force: true });
         console.log("Models are synchronized");
       } catch (err) {
-        console.log('Error!')
-        console.log(err)
+        console.log("Error!");
+        console.log(err);
       }
     }
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
-}
+};
 
-dbCheck()
+dbCheck();
 
-export default db
+export default db;
