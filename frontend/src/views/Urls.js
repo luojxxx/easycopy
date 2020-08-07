6c2e11581ac96172758025397f657a2f4a5d1b31
luojxxx <luojxxx@users.noreply.github.com>
2020-08-07 11:05:35 -0700

    Plug frontend into deleteUserUrl

--- frontend/src/views/Urls.js
+++ frontend/src/views/Urls.js
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from "react";
 import { Flex, Box, Heading, Text } from "rebass";
 import { Label } from "@rebass/forms";
 import axios from "axios";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import dayjs from "dayjs";
+import { FiX } from "react-icons/fi";
 
 import { api } from "../constants";
 import Template from "../components/Template";
 import Input from "../components/Input";
 import Button from "../components/Button";
 
-import { dateFormat } from '../constants'
+import { dateFormat } from "../constants";
 
 const Urls = () => {
-  const [urls, setUrls] = useState([])
-  const [page, setPage] = useState(0)
+  const [urls, setUrls] = useState([]);
+  const [page, setPage] = useState(0);
   useEffect(() => {
     try {
       const result = axios({
@@ -30,6 +31,21 @@ const Urls = () => {
       console.log(err);
     }
   }, [page]);
+  const handleDelete = async (urlId) => {
+    try {
+      await axios({
+        method: "post",
+        url: api + "/deleteuserurl",
+        data: {
+          userToken: localStorage.getItem("userToken"),
+          urlId: urlId,
+        },
+      });
+      setUrls(urls.filter((ele) => ele.urlId !== urlId));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <Template>
       <Flex flexDirection="column" alignItems="center" width={1}>
@@ -38,13 +54,29 @@ const Urls = () => {
         </Heading>
         <Flex flexDirection="column">
           {urls.map((ele) => (
-            <Link to={ele.url}>
-              <Flex flexDirection="row" justifyContent="flex-start">
-                <Text color="primary">{dayjs(ele.createdAt).format(dateFormat)}</Text>&nbsp;
-                <Text color="primary">{ele.url} </Text>&nbsp;
-                <Text color="primary">{ele.content} </Text>
-              </Flex>
-            </Link>
+            <Flex
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Link to={ele.url}>
+                <Flex flexDirection="row" alignItems="center">
+                  <Text color="primary">
+                    {dayjs(ele.createdAt).format(dateFormat)}
+                  </Text>
+                  &nbsp;
+                  <Text color="primary">{ele.url} </Text>&nbsp;
+                  <Text color="primary">{ele.content} </Text>
+                </Flex>
+              </Link>
+              <Text color="primary">
+                <FiX
+                  size={24}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleDelete(ele.urlId)}
+                />
+              </Text>
+            </Flex>
           ))}
         </Flex>
       </Flex>
