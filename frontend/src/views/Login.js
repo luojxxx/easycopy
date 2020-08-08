c82e282a44dbf5eb13ee4ab311619ef423fd501a
luojxxx <luojxxx@users.noreply.github.com>
2020-08-08 11:12:25 -0700

    Update SignUp and Login to capture enter key

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -14,7 +14,8 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       const result = await axios({
         method: "post",
@@ -24,12 +25,12 @@ const Login = () => {
           password: password,
         },
       });
-      const { data } = result
+      const { data } = result;
       localStorage.setItem("userToken", data.userToken);
       localStorage.setItem("userName", data.user.userName);
       localStorage.setItem("email", data.user.email);
       localStorage.setItem("emailVerified", data.user.emailVerified);
-      history.push('/urls')
+      history.push("/urls");
     } catch (err) {
       console.log(err);
       setMessage("Bad login");
@@ -37,35 +38,37 @@ const Login = () => {
   };
   return (
     <Template>
-      <Flex flexDirection="column" alignItems="center" width={1}>
-        <Heading color="primary" pb={3}>
-          Login
-        </Heading>
-        <Box width={0.75} pb={3}>
-          <Label htmlFor="user">Email</Label>
-          <Input
-            id="user"
-            name="user"
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </Box>
-        <Box width={0.75} pb={4}>
-          <Label htmlFor="user">Password</Label>
-          <Input
-            id="user"
-            name="user"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </Box>
-        <Button width={0.75} onClick={handleSubmit} mb={2}>
-          Submit
-        </Button>
-        <Text color="primary">{message}</Text>
-      </Flex>
+      <form onSubmit={handleSubmit}>
+        <Flex flexDirection="column" alignItems="center" width={1}>
+          <Heading color="primary" pb={3}>
+            Login
+          </Heading>
+          <Box width={0.75} pb={3}>
+            <Label htmlFor="user">Email</Label>
+            <Input
+              id="user"
+              name="user"
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </Box>
+          <Box width={0.75} pb={4}>
+            <Label htmlFor="user">Password</Label>
+            <Input
+              id="user"
+              name="user"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </Box>
+          <Button width={0.75} mb={2} type="submit">
+            Submit
+          </Button>
+          <Text color="primary">{message}</Text>
+        </Flex>
+      </form>
     </Template>
   );
 };
--- frontend/src/views/SignUp.js
+++ frontend/src/views/SignUp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Flex, Box, Heading, Text } from "rebass";
 import { Label } from "@rebass/forms";
 import axios from "axios";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 import { api } from "../constants";
 import Template from "../components/Template";
@@ -16,7 +16,8 @@ const SignUp = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [userName, setUserName] = useState("");
   const [message, setMessage] = useState("");
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     setMessage("");
     if (email === "") {
       setMessage("Email can't be blank");
@@ -40,7 +41,7 @@ const SignUp = () => {
         localStorage.setItem("userName", data.user.userName);
         localStorage.setItem("email", data.user.email);
         localStorage.setItem("emailVerified", data.user.emailVerified);
-        history.push('/')
+        history.push("/");
       } catch (err) {
         console.log(err);
       }
@@ -48,54 +49,56 @@ const SignUp = () => {
   };
   return (
     <Template>
-      <Flex flexDirection="column" alignItems="center" width={1}>
-        <Heading color="primary" pb={3}>
-          SignUp
-        </Heading>
-        <Box width={0.75} pb={3}>
-          <Label htmlFor="user">Email</Label>
-          <Input
-            id="user"
-            name="user"
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </Box>
-        <Box width={0.75} pb={3}>
-          <Label htmlFor="user">Password (repeat twice to confirm)</Label>
-          <Box pb={2}>
+      <form onSubmit={handleSubmit}>
+        <Flex flexDirection="column" alignItems="center" width={1}>
+          <Heading color="primary" pb={3}>
+            SignUp
+          </Heading>
+          <Box width={0.75} pb={3}>
+            <Label htmlFor="user">Email</Label>
+            <Input
+              id="user"
+              name="user"
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </Box>
+          <Box width={0.75} pb={3}>
+            <Label htmlFor="user">Password (repeat twice to confirm)</Label>
+            <Box pb={2}>
+              <Input
+                id="user"
+                name="user"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </Box>
             <Input
               id="user"
               name="user"
               type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
             />
           </Box>
-          <Input
-            id="user"
-            name="user"
-            type="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-          />
-        </Box>
-        <Box width={0.75} pb={4}>
-          <Label htmlFor="user">UserName (can be blank and changed)</Label>
-          <Input
-            id="user"
-            name="user"
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-            value={userName}
-          />
-        </Box>
-        <Button width={0.75} onClick={handleSubmit} mb={2}>
-          Submit
-        </Button>
-        <Text color="primary">{message}</Text>
-      </Flex>
+          <Box width={0.75} pb={4}>
+            <Label htmlFor="user">UserName (can be blank and changed)</Label>
+            <Input
+              id="user"
+              name="user"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+              value={userName}
+            />
+          </Box>
+          <Button width={0.75} onClick={handleSubmit} mb={2} type="submit">
+            Submit
+          </Button>
+          <Text color="primary">{message}</Text>
+        </Flex>
+      </form>
     </Template>
   );
 };
