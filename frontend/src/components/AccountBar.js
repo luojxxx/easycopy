0b47819f5b0394d7b44856157ae4d69667f4d369
luojxxx <luojxxx@users.noreply.github.com>
2020-08-08 15:07:05 -0700

    Set frontend to run checkUser endpoint on mount

--- frontend/src/components/AccountBar.js
+++ frontend/src/components/AccountBar.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Text } from "rebass";
 import axios from 'axios'
@@ -9,6 +9,27 @@ import { api } from '../constants'
 const AccountBar = () => {
   const history = useHistory();
   const [loggedin, setLoggedin] = useState(localStorage.getItem("email") !== null)
+  useEffect(() => {
+    const updateUserData = async () => {
+      try {
+        const result = await axios({
+          method: "post",
+          url: api + "/checkUser",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+          data: {},
+        });
+        const { data } = result;
+        localStorage.setItem("userName", data.user.userName);
+        localStorage.setItem("email", data.user.email);
+        localStorage.setItem("emailVerified", data.user.emailVerified);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    updateUserData()
+  }, [0])
   const handleSignOut = async () => {
     try {
       const result = await axios({
