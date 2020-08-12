af9c9a462a44aaeb8df09b1c07b493a669bd4316
luojxxx <luojxxx@users.noreply.github.com>
2020-08-12 12:04:27 -0700

    Fix bug in authentication middleware

--- src/app.js
+++ src/app.js
@@ -49,7 +49,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(async function (req, res, next) {
-  if ("authorization" in req.headers) {
+  if (!("authorization" in req.headers)) {
+    next();
+  } else {
     const userToken = req.headers.authorization.replace("Bearer ", "");
     const user = await UserToken.findOne({
       where: { userToken: userToken },
