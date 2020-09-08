0698f468923dde8dc3a698eb7351d3a499307e56
luojxxx <luojxxx@users.noreply.github.com>
2020-09-08 14:07:45 -0700

    Simplify backend error handling

--- src/app.js
+++ src/app.js
@@ -78,13 +78,9 @@ app.use(Sentry.Handlers.errorHandler());
 app.use(function (err, req, res, next) {
   console.log(err)
 
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
+  res.status(500);
+  res.send('Error');
 });
 
 module.exports = app;
